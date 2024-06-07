8239a87fd197d82abde0490d5ea0f6abe95132f4
Muddathir Julie <107460369+Muddaj7@users.noreply.github.com>
2024-06-07 19:37:25 +0200

    removd unused files

--- purgecss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    content: ['c:\Users\Admin\Documents\Websites\Online-Portfolio\index.html'],
-    css: ['c:\Users\Admin\Documents\Websites\Online-Portfolio\styles.css'],
-    output: 'c:\Users\Admin\Documents\Websites\Online-Portfolio\newStyle.css'
-};
