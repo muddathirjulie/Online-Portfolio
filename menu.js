d6cfda9fdeafb317608e2a1d6fa8a715b70180d0
Muddathir Julie <107460369+Muddaj7@users.noreply.github.com>
2024-11-21 13:01:16 +0200

    redid website

--- menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger");
-    const navList = document.getElementById("nav-list");
-
-    // Toggle navigation menu on hamburger click
-    hamburger.addEventListener("click", function () {
-        navList.classList.toggle("active");
-    });
-});
